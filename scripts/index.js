1d2ab49ac3826d74dd25a39b61414499ac667935
Cris <cfads2001@gmail.com>
2021-10-04 12:39:35 -0300

    add editais e projetos

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
-const instituto = document.querySelectorAll('#instituto li a')
 
+//Navegação
+const instituto = document.querySelectorAll('#instituto li a')
 instituto.forEach( item =>{
     item.addEventListener("click",()=>{
         window.location.href="index.html";
     });
 })
+
+const projetos = document.querySelector('#projetos');
+projetos.addEventListener("click",()=>{
+    window.location.href="projetos.html"
+})
+
+const editais = document.querySelector('#editais');
+editais.addEventListener("click",()=>{
+    window.location.href="editais.html"
+})
+
+const parceiros = document.querySelector('#parceiros')
+parceiros.addEventListener("click",()=>{
+    window.location.href="parceiros.html"
+})
+
 
 
